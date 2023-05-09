584e00a8f32924e3f50e602b62027b00f179e100
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-09 22:11:03 +0600

    first commit

---
+++ _steps.js
@@ -0,0 +1,66 @@
+/* 
+setap initial project
+...........................
+1. npm init -y 
+2.npm i express cors mongodb dotenv
+3.create index.js and send command nodemon index.js to set it on watchmood
+4.require dotenv and create foder name .env 
+5. to set up these env files = process.env.DB_NAME
+start coding
+*/
+
+// make sure you have managed mongodb roles 
+
+/* 
+INSERT ............///////////////////////
+
+to upload data to the server 
+server site 
+................
+step1
+purpose : post the data to the database
+1.use app.post()  make sure it is async await
+
+client site
+................
+step 2
+ purpose : send data to the server 
+1.use fetch('', {method, headers,body}) 
+..................
+step 3
+go to  
+1.'node mongodb crud' > usage example > insert a document 
+2.make a collection inside try{}
+3.inside app.post() use insertOne 
+
+*/
+
+
+/* 
+as we finished postet data to our database, now we have to show the data to our ui , its called read . and for that we use app.get() details in below - 
+
+TO SHOW THE DATA TO THE UI 
+
+FIND....................../////////////
+STEP 1 
+SERVER SITE 
+................... 
+1.app.get() async await must be
+
+
+
+
+
+
+
+
+
+
+
+
+CLIENT SITE 
+1.fetch(url) or loader to the route path 
+2.make component , map distracture to shaw the data to the ui 
+
+
+*/
---
+++ index.js
@@ -0,0 +1,87 @@
+// requiring important things
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 6969 ;
+
+
+//  middleaware 
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+   
+    const coffeCollection = client.db("coffeDB").collection('coffe');
+
+    // to read the data 
+    app.get('/coffe', async(req, res)=>{
+      const cursor  = coffeCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+  
+    
+    //post - upload data to the database 
+    app.post('/coffe', async(req, res)=>{
+        const newCoffe = req.body 
+        console.log(newCoffe)
+        const result = await coffeCollection.insertOne(newCoffe)
+        res.send(result)
+    }) 
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+// route server path 
+app.get('/' , (req, res)=>{
+    res.send('coffe server is running')
+})
+
+
+
+
+
+
+
+
+// listen
+app.listen(port,()=>{
+    console.log(`cofee at the port of : ${port}`)
+})
